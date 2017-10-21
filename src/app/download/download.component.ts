b268fda01ff0e46b9d65c72a7c886f75386d0794
David <dev.d4v@gmail.com>
2017-10-21 21:09:20 +0200

    add download component

--- src/app/download/download.component.ts
+++ src/app/download/download.component.ts
@@ -3,10 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-download',
   templateUrl: './download.component.html',
-  styleUrls: ['./download.component.scss']
+  styleUrls: ['./download.component.scss'],
 })
 export class DownloadComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit() {
