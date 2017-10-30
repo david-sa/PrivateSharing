ea3fc7b101ebb032953b77bc3d7e2270f65c1f43
David <dev.d4v@gmail.com>
2017-10-30 09:20:17 +0100

    add docs component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
@@ -33,6 +34,7 @@ import { PopoverComponent } from './popover/popover.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
     NgbModule.forRoot(),
     AppRoutingModule
   ],
--- src/app/docs/docs.component.ts
+++ src/app/docs/docs.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-docs',
@@ -7,9 +10,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DocsComponent implements OnInit {
 
-  constructor() { }
+  public model: any;
+  statesWithFlags;
+  @ViewChild("tabset") tabset;
+  @ViewChild("acc1") acc1;
+  @ViewChild("acc2") acc2;
+  
+  constructor() {}
 
-  ngOnInit() {
+  search = (text$: Observable<string>) =>
+    text$
+      .debounceTime(200)
+      .map(term => term === '' ? []
+        : this.statesWithFlags.filter(v => v.name.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10));
+
+  formatter = (x: {name: string}) => x.name;
+
+  updateModel( e ) {
+    this.model = e;
+    if (this.model.name ) {
+      this.tabset.select( this.model.tab );
+      if( !this.model.accordion.panels.find( p => p.id === this.model.panel ).isOpen ){
+        this.model.accordion.toggle( this.model.panel );
+      }
+      document.querySelector('#' + this.model.panel + '-header' ).scrollIntoView();       
+    } 
   }
 
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    console.log("view init");
+    this.statesWithFlags = [
+      // User manual
+      {'name': 'Overview', 'tab': 'tab1', 'accordion': this.acc1, 'panel': 't1-static-1' },
+      {'name': 'Download', 'tab': 'tab1', 'accordion': this.acc1, 'panel': 't1-static-2' },
+      {'name': 'Adding content', 'tab': 'tab1', 'accordion': this.acc1, 'panel': 't1-static-3' },
+      {'name': 'Adding contacts', 'tab': 'tab1', 'accordion': this.acc1, 'panel': 't1-static-4' },
+      {'name': 'Creating groups', 'tab': 'tab1', 'accordion': this.acc1, 'panel': 't1-static-5' },
+      {'name': 'Sharing content', 'tab': 'tab1', 'accordion': this.acc1, 'panel': 't1-static-6' },
+      {'name': 'Setting up a custom server', 'tab': 'tab1', 'accordion': this.acc1, 'panel': 't1-static-7' },
+      // Developers guide
+      {'name': 'Overview', 'tab': 'tab2', 'accordion': this.acc2, 'panel': 't2-static-1' },
+      {'name': 'Getting started', 'tab': 'tab2', 'accordion': this.acc2,  'panel': 't2-static-2' },
+      {'name': 'Fundamentals', 'tab': 'tab2', 'accordion': this.acc2,  'panel': 't2-static-3' },
+      {'name': 'Encryption methods', 'tab': 'tab2', 'accordion': this.acc2,  'panel': 't2-static-4' },
+      {'name': 'Techniques', 'tab': 'tab2', 'accordion': this.acc2,  'panel': 't2-static-5' },
+      {'name': 'API', 'tab': 'tab2', 'accordion': this.acc2,  'panel': 't2-static-6' }
+    ];
+  }
 }
